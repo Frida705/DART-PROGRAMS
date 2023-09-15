454656df3c068fa7471d5dceb021d040afaf6481
FRIDA WANJA <nyagafrida02@gmail.com>
2023-09-15 20:54:40 +0300

    first commit

---
+++ conditional.py
@@ -0,0 +1,28 @@
+# if condition :
+# print("output")
+
+
+hungry = True
+
+if hungry:
+    print("Eat some food")
+Num = 4
+
+if Num >= 32:
+    print("the value of num is greater than 20")
+else:
+    print("Num is less than 32")
+    
+marks=67
+    
+if marks>=90:
+        
+        print("scored an A")
+        
+elif marks>=80:
+    print('scored a B')
+
+elif marks<=70:
+    print("csored a C")
+else:
+    print("failed")
